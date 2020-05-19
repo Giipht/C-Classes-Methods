cd94e26f00b1105a4450a5ba6479f987b200836a
Gift Chukwuemeka <60383675+Giipht@users.noreply.github.com>
2020-05-19 21:56:42 +0100

    Add files via upload

---
+++ Animal.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CLassesMethods
+{
+    class Animal
+    {
+        //CLASS VARIABLES
+        public static int Count = 0;
+
+        public string name;
+        public int age;
+        public float happiness;
+
+        //CLASS CONSTRUCTORS
+        public Animal()
+        {
+            name = "Blacky";
+            age = 6;
+            happiness = 0.5f;
+
+            Count++;
+        }
+
+        public Animal(string _name, int _age, float _happiness)
+        {
+            name = _name;
+            age = _age;
+            happiness = _happiness;
+
+            Count++;
+        }
+
+        //CLASS METHODS
+        public void Print()
+        {
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Age: " + age);
+            Console.WriteLine("Happiness: " + happiness);
+        }
+    }
+}
