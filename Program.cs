f8eb194ac0ceb384111d58b40d54309861fdbe89
Gift Chukwuemeka <60383675+Giipht@users.noreply.github.com>
2020-05-19 21:56:02 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CLassesMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal dog = new Animal();
+            dog.Print();
+
+            Console.WriteLine();
+
+            Animal cat = new Animal("Mr. Garfield", 10, 0.8f);
+            cat.Print();
+
+            Console.WriteLine();
+
+            Animal rabbit = new Animal("Mr. Spotty", 4, 1.2f);
+            rabbit.Print();
+
+            Console.WriteLine();
+
+            Console.WriteLine("Number of Animals: " + Animal.Count);
+
+            Console.ReadLine();
+        }
+    }
+}
